9787f1e4169d817304bf25058381fe0518982d0d
XMuminov <xurshidbek4343@gmail.com>
2023-02-06 18:29:50 +0500

    changes

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const display = document.querySelector("#display"),
-    buttons = document.querySelectorAll("button"),
-    btnOperators = document.querySelectorAll(".btn-operator");
+const display = document.querySelector("#display"),
+buttons = document.querySelectorAll("button");
 
-  buttons.forEach((item) => {
+buttons.forEach((item) => {
+item.onclick = (e) => {
+  e.preventDefault();
+  // console.log(item.classList.contains("btn-operator"));
 
-    item.onclick = (e) => {
-      e.preventDefault();
-      // console.log(item.classList.contains("btn-operator"));
+  let val = display.innerHTML;
+  if (!isNumber(val[val.length - 1]) && !isNumber(item.id)) {
+    display.innerHTML = display.innerHTML.substring(0, val.length - 1);
+  }
 
-
-      if (item.id == "clear") {
-        display.innerText = "";
-      } else if (display.innerText != "0" && item.id == "equal") {
-        display.innerText = eval(display.innerText);
-      } else if (display.innerText == "" && item.id == "equal") {
-        display.innerText = "Empty!";
-        setTimeout(() => (display.innerText = ""), 2000);
-      } else if (item.id == "Deg") {
-        // let string = display.innerText.toString();
-        // display.innerText = string.slice(0, string.length - 1);
-        display.innerText = display.innerText.slice(0, -1)
-      } else {
-        if(item.classList.contains('btn-operator')) {
-          console.log(item)
-          display.innerText += item.id;
-        } else {
-          display.innerText += item.id;
-        }
-      }
-    };
-  });
+  if (item.id == "clear") {
+    display.innerText = "";
+  } else if (display.innerText != "0" && item.id == "equal") {
+    display.innerText = eval(display.innerText);
+  } else if (display.innerText == "" && item.id == "equal") {
+    display.innerText = eval(nums.split("").reverse().join(""));
+    display.innerText = "Empty!";
+    setTimeout(() => (display.innerText = ""), 2000);
+  } else if (item.id == "Deg") {
+    // let string = display.innerText.toString();
+    // display.innerText = string.slice(0, string.length - 1);
+    display.innerText = display.innerText.slice(0, -1);
+  } else {
+    display.innerText += item.id;
+  }
+};
 });
+let isNumber = function isNumber(value) {
+return typeof +value === "number" && isFinite(value);
+};
